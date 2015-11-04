d371d54cdc5dd2ef20ebd782526fb642d9dc8c0a
Daniel Kronovet <kronovet@gmail.com>
2015-11-04 01:17:47 -0500

    commented out test deploy code

--- thena-infra/recipes/deploy.rb
+++ thena-infra/recipes/deploy.rb
@@ -5,41 +5,41 @@
 # Copyright (c) 2015 Daniel Kronovet, All Rights Reserved.
 
 
-# include_recipe 'deploy'
+include_recipe 'deploy'
 
-# node[:deploy].each do |application, deploy|
-#   opsworks_deploy_dir do
-#     user deploy[:user]
-#     group deploy[:group]
-#     path deploy[:deploy_to]
-#   end
+node[:deploy].each do |application, deploy|
+  opsworks_deploy_dir do
+    user deploy[:user]
+    group deploy[:group]
+    path deploy[:deploy_to]
+  end
 
-#   opsworks_deploy do
-#     deploy_data deploy
-#     app application
-#   end
-# end
-
-# execute 'update packages' do
-#     cwd node['thena-infra']['app_dir']
-#     command 'pip install -r requirements.txt'
-# end
-
-## FOR TESTING IN TEST KITCHEN
-python_pip 'uwsgi'
-
-directory node['thena-infra']['app_dir'] do
-  recursive true
+  opsworks_deploy do
+    deploy_data deploy
+    app application
+  end
 end
 
-file "#{node['thena-infra']['app_dir']}/wsgi.py" do
-    content "def app(env, start_response):
-    start_response('200 OK', [('Content-Type','text/html')])
-    return [b'Hello Thena\\n']"
-    owner node['thena-infra']['uwsgi_user']
-    group node['thena-infra']['uwsgi_group']
+execute 'update packages' do
+    cwd node['thena-infra']['app_dir']
+    command 'pip install -r requirements.txt'
 end
-## / END TESTING
+
+# ## FOR TESTING IN TEST KITCHEN
+# python_pip 'uwsgi'
+
+# directory node['thena-infra']['app_dir'] do
+#   recursive true
+# end
+
+# file "#{node['thena-infra']['app_dir']}/wsgi.py" do
+#     content "def app(env, start_response):
+#     start_response('200 OK', [('Content-Type','text/html')])
+#     return [b'Hello Thena\\n']"
+#     owner node['thena-infra']['uwsgi_user']
+#     group node['thena-infra']['uwsgi_group']
+# end
+# ## / END TESTING
 
 service 'thena-uwsgi' do
   provider Chef::Provider::Service::Upstart
